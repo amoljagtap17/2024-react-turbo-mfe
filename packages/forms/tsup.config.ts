4e29d3bb40d65e04412bb42361b8f2887231fa69
amoljagtap17 <amoljagtap17@gmail.com>
2024-08-27 16:36:38 +0530

    react-turbo-mfe: add forms package

---
+++ packages/forms/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, type Options } from "tsup";
+
+export default defineConfig((options: Options) => ({
+  entry: ["./src/index.tsx"],
+  format: ["cjs", "esm"],
+  dts: true,
+  external: ["react", "react-dom"],
+  ...options,
+}));
