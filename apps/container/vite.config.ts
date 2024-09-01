abfe89a7eb786f7ffb5cee53bab55db0a3c24cac
amoljagtap17 <amoljagtap17@gmail.com>
2024-09-01 11:50:11 +0530

    react-turbo-mfe: add federation config

--- apps/container/vite.config.ts
+++ apps/container/vite.config.ts
@@ -1,6 +1,34 @@
+import federation from "@originjs/vite-plugin-federation";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "container",
+      remotes: {
+        summaryApp: "summary@http://localhost:3002/remoteEntry.js",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    host: "localhost",
+    port: 3001,
+    strictPort: true,
+    open: false,
+  },
+  preview: {
+    host: "localhost",
+    port: 3001,
+    strictPort: true,
+    open: false,
+  },
 });
--- apps/summary/vite.config.ts
+++ apps/summary/vite.config.ts
@@ -1,6 +1,35 @@
+import federation from "@originjs/vite-plugin-federation";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "summary",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Dashboard": "./src/components/sections/dashboard/Dashboard",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    host: "localhost",
+    port: 3002,
+    strictPort: true,
+    open: false,
+  },
+  preview: {
+    host: "localhost",
+    port: 3002,
+    strictPort: true,
+    open: false,
+  },
 });
